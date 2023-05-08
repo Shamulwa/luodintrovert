9a5d9484af9cc838d3e3379a299488707c36973c
Sham Mulwa <shammulwa@gmail.com>
2023-05-08 18:50:25 +0300

    Update themeSwitch.js
    About page update

--- components/themeSwitch.js
+++ components/themeSwitch.js
@@ -23,9 +23,9 @@ const ThemeSwitch = () => {
         name="themeSwitch"
         value={theme}
         onChange={e => setTheme(e.target.value)}>
-        {/* <option value="system">System</option> */}
         <option value="dark">Dark</option>
         <option value="light">Light</option>
+        <option value="system">Auto</option>
       </select>
     </div>
   );
